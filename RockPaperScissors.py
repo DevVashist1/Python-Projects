4db137223893efcdd1485322ba9b269dd2902cd3
Dev Vashist <67896849+DevVashist1@users.noreply.github.com>
2024-06-25 09:52:38 +0530

    Added to repo
    -Improved the code.
    -Added rounds and score.
    -Removed any input error chances 
    
    Any suggestion in this project will be accepted gladly.

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,51 +1,73 @@
 import random
 
 #Starts the game
-print("It's a Rock Paper Scissors Game.")
+print("\nIt's a Rock Paper Scissors Game.")
 
+#Tuple for computer and user to choose
 sequence = ("rock", "paper", "scissors")
-entry = input("Enter the game(y/n): ")
-entry = entry.lower()
+options =("y","n")
 
-while True :
-    if entry == "y" :
+#Rounds of game
+round = 1
+
+#User score in game 
+user_score = 0
+
+#Computer score in game
+computer_score = 0
+
+enter_game = input("\nWould you like to play Rock Paper Scissors? (y/n): ").lower()
+
+#Checks for user input if it is valid or not
+while enter_game not in options :
+    enter_game = input ("Please enter a valid answer (y/n): ").lower()
+
+#Looping if user wants to play
+while enter_game == "y" :
+
+    #Random choice for computer out of tuple sequence
+    computer_choice = random.choice(sequence)
+
+    print("ROUND ",round)
+    
+    #Asking user for user's choice out of sequence
+    user_choice = input("\nChoose your choice (rock/paper/scissors): ").lower()
+    
+    #Checking if user input is out of tuple sequence or not
+    while user_choice not in sequence :
+        user_choice = input("Please choose a valid choice (rock/paper/scissors): ").lower()
         
-        computer = random.choice(sequence)
 
-        tries = input("Enter your choice (rock, paper or scissors): ")
-        tries = tries.lower()
-
-        if computer == "rock" and tries == "paper" :
-            print ("You won!")
-            print("Computer : ",computer,"\n","You : ",tries)
-        elif computer == "rock" and tries == "scissors" :
-            print ("You lost!")
-            print("Computer : ",computer,"\n","You : ",tries)
-        elif computer == "paper" and tries == "rock" :
-            print ("You lost!")
-            print("Computer : ",computer,"\n","You : ",tries)
-        elif computer == "paper" and tries == "scissors" :
-            print ("You won!")
-            print("Computer : ",computer,"\n","You : ",tries)
-        elif computer == "scissors" and tries == "rock" :
-            print ("You won!")
-            print("Computer : ",computer,"\n","You : ",tries)
-        elif computer == "scissors" and tries == "paper" :
-            print("You lost!")
-            print("Computer : ",computer,"\n","You : ",tries)
-        elif computer == tries:
-            print("It's a tie.")
-            print("Computer : ",computer,"\n","You : ",tries)
-        else :
-            print("Enter a Valid Input (rock/paper/scissors).\n")
+    #Condition when user win against the computer
+    if (user_choice == "rock" and computer_choice == "scissors") or (user_choice == "paper" and computer_choice == "rock") or (user_choice == "scissors" and computer_choice == "paper") :
         
-        entry = input("Try again?(y/n) : ")
-        entry = entry.lower()
+        print("You won!")
+        round += 1
+        user_score += 1
 
-    elif entry == "n" :
-        print("See you soon! Bye.")
-        break
+    #Condition where user and computer have a tie
+    elif user_choice == computer_choice :
+        
+        print("It's a tie.")
+        round += 1
+    
+    #Condition when user lost against computer
     else :
-        print("Enter a Valid Input (y/n).")
-        entry = input("Try again?(y/n) : ")
-        continue
+        round += 1
+        computer_score += 1
+        print ("You lost!")
+        
+
+    #Choices and Score display    
+    print("Computer: ", computer_choice,"\n","    You: ", user_choice)
+    print("\nComputer: ", computer_score,"\n","    You: ", user_score)
+          
+    enter_game = input("\nDo you like to play again?(y/n)").lower()
+    
+    #Checks for user input if it is valid or not
+    while enter_game not in options :
+        enter_game = input ("Please enter a valid answer (y/n): ")
+
+#Condition when user denies to play 
+if enter_game == "n" :
+    print("\nSee you soon! \nBye. \n")
